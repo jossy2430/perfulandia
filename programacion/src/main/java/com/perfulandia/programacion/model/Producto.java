b84087d15d2343cc98be505bf4278adec8b13818
joselyn gallardo <jo.gallardo@duocuc.cl>
2025-05-07 08:13:09 -0400

    Agregar entidadades (ej: Sucursal, Producto, Empleado)

---
+++ programacion/src/main/java/com/perfulandia/programacion/model/Producto.java
@@ -0,0 +1,32 @@
+package com.perfulandia.programacion.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "producto")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idProducto;
+
+    @Column(nullable = false, length = 50)
+    private String nombreProducto;
+
+    @Column(nullable = false, length = 255)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Integer precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+}
